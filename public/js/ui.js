ad40098ef9eea3ea65cc72423144ab9287e445a3
omercikan <omercikan36@gmail.com>
2024-07-31 16:21:55 +0300

    fadehub-hairdresser

---
+++ public/js/ui.js
@@ -0,0 +1,10 @@
+function UI() {
+    this.responsiveNavbar = document.querySelector(".responsive-navbar");
+    this.mainNav = document.querySelector(".main-nav");
+    this.bars = document.querySelector(".bars");
+    this.close = document.querySelector(".close");
+    this.navItems = document.querySelectorAll(".nav-items a");
+    this.pricingCol = document.querySelectorAll(".pricing-col");
+    this.accordionCol = document.querySelectorAll(".accordion-col");
+    this.customerRow = document.querySelector(".customer-comments-slider-row");
+};
